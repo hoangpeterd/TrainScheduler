1850fb5617a3713a783e7ad0715871ccd9cbe13a
Peter Hoàng <peterhoang@utexas.edu>
2016-12-09 13:19:18 -0600

    html page complete, firebase initialized

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+// initializing firebase
+var config = {
+	apiKey: "AIzaSyCi2duyEoZsEZ2bYLjB2pop7yqbukV-tFE",
+    authDomain: "trainscheduler-c7e6d.firebaseapp.com",
+    databaseURL: "https://trainscheduler-c7e6d.firebaseio.com",
+    storageBucket: "trainscheduler-c7e6d.appspot.com",
+    messagingSenderId: "689950630388"
+};
+firebase.initializeApp(config);
+// end initializing firebase
+
+});
+
