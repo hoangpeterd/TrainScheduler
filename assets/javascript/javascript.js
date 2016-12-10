6c11093aea1865b03bbcb90610804fdc2f75ad7b
Peter Hoàng <peterhoang@utexas.edu>
2016-12-09 22:51:11 -0600

    firebase now stores added data

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -9,7 +9,49 @@ var config = {
     messagingSenderId: "689950630388"
 };
 firebase.initializeApp(config);
+
+var database = firebase.database();
 // end initializing firebase
+
+// button for adding trains
+$("#add-train-btn").on("click", function() {
+
+	// grabs user input
+	var trnName = $("#train-name-input").val().trim();
+	var trnDestination = $("#destination-input").val().trim();
+	var trnFirst = $("#first-input").val().trim();
+	var trnFrequency = $("#frequency-input").val().trim();
+
+	// creates local "temporary" object for holding train data
+	var newTrain = {
+		name: trnName,
+		destination: trnDestination,
+		first: trnFirst,
+		frequency: trnFrequency
+	};
+
+	// uploads employee data to the database
+	database.ref().push(newTrain);
+
+	//logs everything to console
+	console.log(newTrain.name);
+	console.log(newTrain.destination);
+	console.log(newTrain.first);
+	console.log(newTrain.frequency);
+
+	// alert
+	alert("Employee successfully added");
+
+	// clears all of the text-boxes
+	$("#train-name-input").val("");
+	$("#destination-input").val("");
+	$("#first-input").val("");
+	$("#frequency-input").val("");
+
+	//prevents moving to new page
+	return false;
+});
+// end button for adding trains
 
 });
 
