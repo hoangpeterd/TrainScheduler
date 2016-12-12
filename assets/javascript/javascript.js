2137e8488ec486ca26edb8dee445138cfc65fd96
Peter Hoàng <peterhoang@utexas.edu>
2016-12-11 23:43:46 -0600

    moment JS completed

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -27,30 +27,25 @@ $("#add-train-btn").on("click", function() {
 
 	// prevents page refresh from happening on the submit event
 	event.preventDefault();
-
 	// grabs user input
 	trnName = $("#train-name-input").val().trim();
 		if (trnName === "") {
-			alert("train name is missing");
-			// $('#alertModal').modal();
+			$('#nameModal').modal();
 			return false;
 		}
 	trnDestination = $("#destination-input").val().trim();
 		if (trnDestination === "") {
-			alert("train desintation is missing");
-			// $('#alertModal').modal();
+			$('#destinationModal').modal();
 			return false;
 		}
 	trnFirst = $("#first-input").val().trim();
 		if (trnFirst === "") {
-			alert("first train time missing");
-			// $('#alertModal').modal();
+			$('#firstModal').modal();
 			return false;
 		}
 	trnFrequency = $("#frequency-input").val().trim();
 		if (trnFrequency === "") {
-			alert("train frequency missing");
-			// $('#alertModal').modal();
+			$('#frequencyModal').modal();
 			return false;
 		}
 
@@ -65,14 +60,8 @@ $("#add-train-btn").on("click", function() {
 	// uploads employee data to the database
 	database.ref().push(newTrain);
 
-	//logs everything to console
-	console.log(newTrain.name);
-	console.log(newTrain.destination);
-	console.log(newTrain.first);
-	console.log(newTrain.frequency);
-
 	// alert modal
-	$('#alertModal').modal();
+	$('#successModal').modal();
 
 	// clears all of the text-boxes
 	$("#train-name-input").val("");
@@ -88,51 +77,36 @@ $("#add-train-btn").on("click", function() {
 // creates a firebase event for adding a train to the database and a row in the html when a user adds an entry
 database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
-	console.log(childSnapshot.val());
-
 	// store everything into a variable
 	var trnName = childSnapshot.val().name;
 	var trnDestination = childSnapshot.val().destination;
 	var trnFirst = childSnapshot.val().first;
 	var trnFrequency = childSnapshot.val().frequency;
 
-	// train info
-	console.log(trnName);	
-	console.log(trnDestination);
-	console.log(trnFirst);
-	console.log(trnFrequency);
-
 	// moment JS implementation
 
 	// converts time to readable format
 	var firstTimeConverted = moment(trnFirst, "HH:mm MM DD YYYY").subtract(1, "years");
-	console.log("first time converted: " + firstTimeConverted);
 
 	// converts the time into minutes
-	var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-	console.log("difference in time: " + diffTime);
+	var diffTime = moment().diff(moment(firstTimeConverted), "m");
 
 	// finds the remainder of the the time converted into minutes
 	var tRemainder = diffTime % trnFrequency;
-	console.log(tRemainder);
 
 	// finds the minutes away until the next train
 	var minAway = trnFrequency - tRemainder;
-	console.log(minAway);
 
-	// displays the minutes away until the next train
+	// mutates the original moment by adding time
 	trnMinAway = moment().add(minAway, "minutes");
-	console.log("minutes away: " + minAway);
 
-	// // displays the time of the next train
-	trnNext = moment(trnMinAway).format("hh:mm A");
-	console.log("next train: " + trnNext);
+	// displays the time of the next train
+	trnNext = moment(trnMinAway).format("HH:mm");
 	// end moment JS implementation
 
 	// add each train's data into the table
-	$("#train-table > tbody").append("<tr><td>" + trnName + "</td><td>" + trnDestination + "</td><td>" + trnFrequency + "</td><td>" + trnNext + "</td><td>" + trnMinAway + "</td></tr>");
+	$("#train-table > tbody").append("<tr><td>" + trnName + "</td><td>" + trnDestination + "</td><td>" + trnFrequency + "</td><td>" + trnNext + "</td><td>" + minAway + "</td></tr>");
 });
 // end firebase event for adding trains from user input to table
 
 });
-
